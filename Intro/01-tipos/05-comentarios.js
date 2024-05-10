9209a91d907727c86d68485ee7dca04066a82509
AGomezUPQ <122045264@upq.edu.mx>
2024-05-09 22:47:58 -0600

    Comentarios
    Se reviso como crear comentarios en JS

---
+++ Intro/01-tipos/05-comentarios.js
@@ -0,0 +1,18 @@
+// asdasdasdasdasdas
+/*
+as
+asdasd
+asdasd
+asdasd
+
+*/
+/**
+ * Estes es un comentario
+ * de 
+ * muchas
+ * lineas
+ * 
+ */
+let number = 42; //Comntarios
+
+console.log(number);
