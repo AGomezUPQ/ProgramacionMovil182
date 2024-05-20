30184c67d66f3668e2510b01b2891fcfbfa81726
AGomezUPQ <122045264@upq.edu.mx>
2024-05-20 15:42:15 -0600

    Avance Pract2

---
+++ Intro/JS4React/index.js
@@ -0,0 +1,36 @@
+//funcion
+/*
+function suma  (a,b){
+    return a+b
+}
+
+*/
+//funcion fatArrow
+/*
+const suma = (a,b)=>{
+    return a+b
+    }
+
+console.log(suma(2,3));
+*/
+
+//fat arrow simplificada
+/*
+const suma = (a,b)=> a+b
+
+console.log(suma(2,3));
+*/
+
+const usuario={
+    nombre: 'ivan',
+    apellidoP: 'Guerra',
+    edad: 36,
+    direccion:{
+        pais: 'Mexico',
+        ciudad: 'Queretaro',
+        calle:'la misma 123'
+    },
+    amigos:['Alex', 'Carlos'],
+    estatus: true,
+    
+}
