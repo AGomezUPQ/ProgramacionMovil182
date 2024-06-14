97f67ea58b2a74f10ef89da444a3f3bc2fa6eaba
AGomezUPQ <122045264@upq.edu.mx>
2024-06-14 15:28:29 -0600

    Se abordaron los temas de Flex y Stylesheet
    Se realizaron modificaciones con una hoja de estilos

--- Hola-mundo/App.js
+++ Hola-mundo/App.js
@@ -4,27 +4,24 @@ import React, {useState} from 'react';
 
 //Funcion Fat arrow, regresar un componente
 
-const Texto=()=>{
+const Texto=({estilo})=>{
   const [contenido, setContenido] = useState('Hola mundo')
   const actualizarContenido = ()=>{
     setContenido('State actualizo este texto')
   }
-  return(<Text onPress={actualizarContenido}>{contenido}</Text>)
+  return(<Text style={[styles.text,estilo]} onPress={actualizarContenido}>{contenido}</Text>)
 }
-
-
 
 
 export default function App() {
   return (
     <View style={styles.container}>
 
+      <Texto estilo={styles.brown}></Texto>
+      <Texto estilo={styles.orange} contenido></Texto>
+      <Texto estilo={styles.black} contenido></Texto>
       
-      <Texto contenido></Texto>
-      <Texto contenido></Texto>
-      
-      
-      
+         
       <Button title='presioname' color="#000000"> </Button>
       <StatusBar style="auto" />
     </View>
@@ -34,8 +31,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    //flexDirection:'row',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
+  text:{
+    color: 'red',
+    fontSize: 25,
+    height: 150,
+    width: 150,
+  },
+  brown:{
+    //flex:1,
+    backgroundColor: 'brown',
+  },
+  orange:{
+    //flex:2,
+    backgroundColor: 'orange',
+  },
+  black:{
+    //flex:3,
+    backgroundColor: 'black',
+  }
 });
+
